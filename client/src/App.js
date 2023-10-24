f1d87a268e324f614e5a4f2a209b4d49ae9ec0ec
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-24 10:44:33 -0400

    theme setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+    const mode = useSelector((state) => state.mode); // This will grab the value from 'state/index.js'. initialState -> mode
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return (
         <div className="app">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/profile/:userId" element={<ProfilePage />} />
-                </Routes>
+                <ThemeProvider theme={theme}>
+                    {/* CssBaseline to reset css to basic for 'mui' */}
+                    <CssBaseline />
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route
+                            path="/profile/:userId"
+                            element={<ProfilePage />}
+                        />
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
 // from 'redux-persist' only
-// To store imformation in local storage
+// To store information in local storage not as session.
 import {
     persistStore,
     persistReducer,
@@ -21,9 +21,32 @@ import {
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistStore(store)}>
+                <App />
+            </PersistGate>
+        </Provider>
     </React.StrictMode>
 );
